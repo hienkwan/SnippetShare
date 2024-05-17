eb197eb48ffb0deb2fe348cb57fbfea2cde3f64f
Hien Quan <hien.quan@pricefx.com>
2024-05-17 09:42:55 +0700

    Initial commit

---
+++ src/main/kotlin/com/example/SnippetShare/SnippetShareApplication.kt
@@ -0,0 +1,11 @@
+package com.example.SnippetShare
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SnippetShareApplication
+
+fun main(args: Array<String>) {
+	runApplication<SnippetShareApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/SnippetShare/SnippetShareApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.SnippetShare
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SnippetShareApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
